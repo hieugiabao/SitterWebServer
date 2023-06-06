592dc33531338a4cc0e3fd1a15f7b5010072a680
hieugiabao <hieugiabao12@gmail.com>
2023-06-06 14:40:58 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const port = process.env.PORT || 5000;
 
 const cors = require("cors");
 
-app.use(cors()); // Use this after the variable declaration
+app.use(
+  cors({
+    origin: "*",
+  })
+); // Use this after the variable declaration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 route(app);
