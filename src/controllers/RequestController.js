d45760ccb9fb709d18acac2d41739520f638bad8
Vandt <dangvan31102001@gmail.com>
2023-06-19 22:55:33 +0700

    fix logic update request state

--- src/controllers/RequestController.js
+++ src/controllers/RequestController.js
@@ -1,104 +1,107 @@
-const Request = require('../models/Request');
+const Request = require("../models/Request");
 
 const allRequests = async (req, res) => {
-    try {
-
-        const requests = await Request.findAll();
-        res.json(requests);
-
-    } catch (error) {
-        console.log("Error searching requests:", error);
-        throw error;
-    }
-}
+  try {
+    const requests = await Request.findAll();
+    res.json(requests);
+  } catch (error) {
+    console.log("Error searching requests:", error);
+    // throw error;
+  }
+};
 
 const findRequestById = async (req, res) => {
-    try {
-        const {requestId} = req.params; // Lấy id từ request parameters
+  try {
+    const { requestId } = req.params; // Lấy id từ request parameters
 
-        const request = await Request.findByPk(requestId); // Tìm request theo id
+    const request = await Request.findByPk(requestId); // Tìm request theo id
 
-        if (!request) {
-            res.status(404).json({error: "Request not found"});
-        } else {
-            res.json(request);
-        }
-    } catch (error) {
-        console.log("Error searching requests:", error);
-        throw error;
+    if (!request) {
+      res.status(404).json({ error: "Request not found" });
+    } else {
+      res.json(request);
     }
+  } catch (error) {
+    console.log("Error searching requests:", error);
+    // throw error;
+  }
 };
 
 const createRequest = async (req, res) => {
-    try {
-        const { start_time, end_time, data, state, parent_id, sitter_id } = req.body;
+  try {
+    const { start_time, end_time, data, state, parent_id, sitter_id } =
+      req.body;
 
-        // Tạo request mới
-        const newRequest = await Request.create({
-            start_time,
-            end_time,
-            data,
-            state,
-            parent_id,
-            sitter_id,
-        });
+    // Tạo request mới
+    const newRequest = await Request.create({
+      start_time,
+      end_time,
+      data,
+      state,
+      parent_id,
+      sitter_id,
+    });
 
-        res.status(201).json(newRequest);
-
-    } catch (error) {
-        console.log("Error searching requests:", error);
-        throw error;
-    }
+    res.status(201).json(newRequest);
+  } catch (error) {
+    console.log("Error searching requests:", error);
+    // throw error;
+  }
 };
 
 const updateRequest = async (req, res) => {
-    try {
-        const { requestId } = req.params;
-        const { start_time, end_time, data, state } = req.body;
+  try {
+    const { requestId } = req.params;
+    const { start_time, end_time, data, state } = req.body;
 
-        const existingRequest = await Request.findByPk(requestId);
-        if (!existingRequest) {
-            return res.status(404).json({ error: 'Request not found' });
-        }
-
-        // Cập nhật thông tin của request
-        existingRequest.start_time = start_time;
-        existingRequest.end_time = end_time;
-        existingRequest.data = data;
-        existingRequest.state = state;
-
-        await existingRequest.save();
-
-        res.status(200).json(existingRequest);
-    } catch (error) {
-        console.log('Error updating request:', error);
-        throw error;
+    const existingRequest = await Request.findByPk(requestId);
+    if (!existingRequest) {
+      return res.status(404).json({ error: "Request not found" });
     }
+
+    // Cập nhật thông tin của request
+    existingRequest.start_time = !start_time
+      ? existingRequest.start_time
+      : start_time;
+
+    existingRequest.end_time = !end_time ? existingRequest.end_time : end_time;
+
+    existingRequest.data = !data ? existingRequest.data : data;
+
+    existingRequest.state = !state ? existingRequest.state : state;
+
+    await existingRequest.save();
+
+    res.status(200).json(existingRequest);
+  } catch (error) {
+    console.log("Error updating request:", error);
+    // throw error;
+  }
 };
 
 const deleteRequest = async (req, res) => {
-    try {
-        const { requestId } = req.params;
+  try {
+    const { requestId } = req.params;
 
-        const existingRequest = await Request.findByPk(requestId);
-        if (!existingRequest) {
-            return res.status(404).json({ error: 'Request not found' });
-        }
-
-        // Xóa request
-        await existingRequest.destroy();
-
-        res.status(200).json({ message: 'Request deleted successfully' });
-    } catch (error) {
-        console.log('Error deleting request:', error);
-        throw error;
+    const existingRequest = await Request.findByPk(requestId);
+    if (!existingRequest) {
+      return res.status(404).json({ error: "Request not found" });
     }
+
+    // Xóa request
+    await existingRequest.destroy();
+
+    res.status(200).json({ message: "Request deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting request:", error);
+    // throw error;
+  }
 };
 
 module.exports = {
-    findRequestById,
-    allRequests,
-    createRequest,
-    updateRequest,
-    deleteRequest
-}
+  findRequestById,
+  allRequests,
+  createRequest,
+  updateRequest,
+  deleteRequest,
+};
